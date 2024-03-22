cd2fb468a05e2890223319d1305c24c4f23f8bde
Matthew Rawson <mrawson@redev.co.uk>
2024-03-22 13:00:20 +0000

    Added Camera with photo save functionality

--- CamerasAndWinForms/Form1.Designer.cs
+++ CamerasAndWinForms/Form1.Designer.cs
@@ -1,4 +1,8 @@
-﻿namespace CamerasAndWinForms
+﻿using OpenCvSharp;
+using Point = System.Drawing.Point;
+using Size = System.Drawing.Size;
+
+namespace CamerasAndWinForms
 {
     partial class Form1
     {
@@ -28,12 +32,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            comboBox1 = new ComboBox();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(329, 284);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 0;
+            comboBox1.DropDown += ComboBox1_Click;
+            comboBox1.SelectedIndexChanged += ComboBox1_SelectedIndexChanged;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(283, 68);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(205, 150);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(329, 255);
+            button1.Name = "button1";
+            button1.Size = new Size(121, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Take Photo";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(595, 284);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(183, 157);
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(comboBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ComboBox comboBox1;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private PictureBox pictureBox2;
     }
 }
--- CamerasAndWinForms/Form1.cs
+++ CamerasAndWinForms/Form1.cs
@@ -1,10 +1,89 @@
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
 namespace CamerasAndWinForms
 {
     public partial class Form1 : Form
     {
+        private VideoCapture _videoCapture = new();
+        private bool _isCameraRunning;
+        private Mat _frame = new();
+
         public Form1()
         {
             InitializeComponent();
+            ComboBox1_Click(new object(), EventArgs.Empty);
+        }
+
+        // Call when a camera is updated, idk what happens when 2 cameras are available
+        private void StartCamera(int cameraIndex)
+        {
+
+            if (!_isCameraRunning)
+            {
+                _videoCapture.Open(cameraIndex);
+                if (_videoCapture.IsOpened())
+                {
+                    _isCameraRunning = true;
+                    Application.Idle += ProcessFrame;
+                }
+                else
+                {
+                    MessageBox.Show("Failed to open camera.");
+                }
+            }
+            else
+            {
+                Application.Idle -= ProcessFrame;
+                _videoCapture.Release();
+                _isCameraRunning = false;
+            }
+        }
+
+        // Fake display a video (only way to do it live in winforms)
+        private void ProcessFrame(object? sender, EventArgs e)
+        {
+            if (_videoCapture.Read(_frame))
+            {
+                pictureBox1.Image = _frame.ToBitmap();
+            }
+        }
+
+        private void ComboBox1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            comboBox1.Items.Add("Select a camera");
+            int deviceIndex = 0;
+            while (true)
+            {
+                using (var capture = new VideoCapture(deviceIndex))
+                {
+                    if (!capture.IsOpened())
+                        break;
+
+                    var deviceName = $"Webcam {deviceIndex + 1}";
+                    comboBox1.Items.Add(deviceName);
+                }
+                deviceIndex++;
+            }
+
+            comboBox1.SelectedIndex = 0;
+        }
+
+        // Call when selecting a new camera
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = comboBox1.SelectedIndex;
+            if (index > 0)
+            {
+                StartCamera(index - 1);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_isCameraRunning)
+                pictureBox2.Image = _frame.ToBitmap();
         }
     }
 }
